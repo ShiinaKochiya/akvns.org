b6a217663b1f76eee4f75aa5501d43b0ba55c32e
DOTANLOC <itcsiu21199@student.hcmiu.edu.vn>
2025-11-12 20:28:32 +0700

    fix testimony

--- src/components/Testimony.tsx
+++ src/components/Testimony.tsx
@@ -19,7 +19,7 @@ type CardData = {
 const reviews: CardData[] = [
     {
         avatar: "/meruko.png",
-        description: "Jennie, chính thất, aka, Vợ cả.",
+        description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec.",
         info: "@jennienguyn",
         name: "Nguyễn Thị Linh Chi",
     },
@@ -27,7 +27,7 @@ const reviews: CardData[] = [
         avatar: "/meruko.png",
         description: "1 con mèo khác, nhưng thua vợ cả.",
         info: "@yuki_chino",
-        name: "Chino",
+        name: "1 cái tên thật là dàiiiiiiiiiiiii",
     },
     {
         avatar: "/meruko.png",
@@ -47,29 +47,27 @@ export default function Testimony() {
     return (
         <div className={"max-w-4xl mx-auto px-6"}>
             <div className={"text-center mb-10"}>
-                <div className={"text-2xl font-bold text-primary mb-2"}>Oguro có bao nhiêu người yêu?</div>
-                <p className={"text-accent-foreground"}>Sau khi bị @Swyrin VAR, Oguro có:</p>
+                <div className={"text-3xl font-bold text-[#F25C5C] mb-2"}>Mọi người nghĩ gì về mình?</div>
+                <p className={"text-accent-foreground text-xl"}>Overwhelming Negative Reviews:</p>
             </div>
 
             <section className={"grid grid-cols-1 sm:grid-cols-2 gap-8"}>
                 {reviews.map(c => (
                     <Card
-                        className={"border border-black shadow-[0_0_20px_4px_rgba(255,0,0,0.1)] "}
+                        className={"border border-accent-foreground shadow-[0_0_20px_4px_rgba(255,0,0,0.2)] "}
                         key={c.info}
                     >
-                        <CardHeader className={"flex flex-col items-center text-center py-6"}>
-                            <div className={"flex flex-cl items-center gap-3"}>
-                                <div className={"rounded-full overflow-hidden border-1 border-black shadow-sm"}>
-                                    <Image
-                                        alt={c.name}
-                                        className={"rounded-full object-cover"}
-                                        height={50}
-                                        src={c.avatar}
-                                        width={50}
-                                    />
-                                </div>
+                        <CardHeader className={"flex flex-col items-left text-left py-2"}>
+                            <div className={"flex gap-2"}>
+                                <Image
+                                    alt={c.name}
+                                    className={"rounded-full object-cover overflow-hidden border-1 border-black shadow-sm"}
+                                    height={50}
+                                    src={c.avatar}
+                                    width={50}
+                                />
                                 <div>
-                                    <CardTitle className={"text-lg font-semibold text-primary"}>
+                                    <CardTitle className={"font-bold text-lg text-primary"}>
                                         {c.name}
                                     </CardTitle>
                                     <p className={"text-sm text-accent-foreground"}>{c.info}</p>
@@ -77,8 +75,10 @@ export default function Testimony() {
                             </div>
                         </CardHeader>
                         <CardContent>
-                            <p className={"text-sm text-accent-foreground leading-relaxed text-center"}>
-                                {c.description}
+                            <p className={"text-base text-accent-foreground leading-relaxed text-left"}>
+                                <span className={"font-bold text-[#F25C5C]"}>"</span>
+                                <span>{c.description}</span>
+                                <span className={"font-bold text-[#F25C5C]"}>"</span>
                             </p>
                         </CardContent>
                     </Card>
